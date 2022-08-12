cc8787b1c6a079105a58ae6623f70c36dcde9494
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-11 22:27:17 -0400

    Adds Wine seeds to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Wine.create!([
+  {name: "Espiral Vinho Verde", producer: "Espiral", vintage: 2018, blend: "Vinho Verde", color: "White", sparkling: true, price: 4.99},
+  {name: "Rebuttel Chardonnay", producer: "Rebuttel", vintage: 2019, blend: "Chardonnay", color: "White", sparkling: false, price: 6.99},
+  {name: "Josephine Cabernet Sauvignon", producer: "Josephine", vintage: 2020, blend: "Cabernet Sauvignon", color: "Red", sparkling: false, price: 6.99},
+  {name: "Cecilia Beretta Prosecco Rosé", producer: "Cecilia Beretta", blend: "Prosecco Rosé", color: "Rosé", sparkling: true, price: 10.99}
+
+
+])
