4fc2d11aee299d343c7dcc7a19bd70b5e67a0603
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-18 19:48:02 -0500

    Adds index method to wines_controller.rb

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,2 +1,8 @@
 class WinesController < ApplicationController
+
+  def index
+    wines = Wine.all
+
+    render json: wines.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # wines
+  get "/wines" => "wines#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
