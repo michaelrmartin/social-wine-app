cbb6011ca524d9724a2aad590f5d27954e3116f2
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-18 20:22:40 -0500

    Adds create method to wines_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  
   # wines
   get "/wines" => "wines#index"
-  post "/wines" = "wines#create"
+  post "/wines" => "wines#create"
   get "/wines/:id" => "wines#show"
   patch "/wines/:id" => "wines#update"
   delete "/wines/:id" =>
