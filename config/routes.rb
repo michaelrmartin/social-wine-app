fb99b64146918808e86e284754206caca4ef2464
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-18 20:05:13 -0500

    Adds remaining restful routes for wines to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # wines
   get "/wines" => "wines#index"
-
+  post "/wines" = "wines#create"
   get "/wines/:id" => "wines#show"
+  patch "/wines/:id" => "wines#update"
+  delete "/wines/:id" =>
+  "wines#destroy"
 
 
 end
