1200dc0595d2909b94c2ba3426d364ef881e0cc4
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-28 15:17:53 -0500

    Updates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
 Wine.create!([
-  {name: "Espiral Vinho Verde", producer: "Espiral", vintage: 2018, blend: "Vinho Verde", price: 4.99},
-  {name: "Rebuttel Chardonnay", producer: "Rebuttel", vintage: 2019, blend: "Chardonnay", price: 6.99},
-  {name: "Josephine Cabernet Sauvignon", producer: "Josephine", vintage: 2020, blend: "Cabernet Sauvignon", price: 6.99},
-  {name: "Cecilia Beretta Prosecco Rosé", producer: "Cecilia Beretta", blend: "Prosecco Rosé", price: 10.99}
+  {name: "Espiral Vinho Verde", producer: "Espiral", vintage: 2018, blend: "Vinho Verde", price: "4.99"},
+  {name: "Rebuttel Chardonnay", producer: "Rebuttel", vintage: 2019, blend: "Chardonnay", price: "6.99"},
+  {name: "Josephine Cabernet Sauvignon", producer: "Josephine", vintage: 2020, blend: "Cabernet Sauvignon", price: "6.99"},
+  {name: "Cecilia Beretta Prosecco Rosé", producer: "Cecilia Beretta", vintage: nil, blend: "Prosecco Rosé", price: "10.99"}
 ])
 
+User.create!([
+  {name: "Test name", email: "test@test.com", password_digest: "$2a$12$.2drYPp4Rrh7tW0uy.qgkuEhDgdtLnnEvThE2YnaXAfLBU/u3Do8."}
+])
+
+Category.create!([
+  {name: "Red"},
+  {name: "White"},
+  {name: "Rosé"},
+  {name: "Orange"},
+  {name: "Sparkling"}
+])
