40b9065ffc04699fd4822c4bfee8ced89a19f662
michaelrmartin <iammichaelmartin@gmail.com>
2022-08-30 20:06:52 -0500

    Updates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+WineOrigin.create!([
+  {wine_id: 1, origin_id: 2},
+  {wine_id: 2, origin_id: 3},
+  {wine_id: 3, origin_id: 1},
+  {wine_id: 4, origin_id: 4},
+  {wine_id: 4, origin_id: 4}
+])
 Wine.create!([
   {name: "Espiral Vinho Verde", producer: "Espiral", vintage: 2018, blend: "Vinho Verde", price: "4.99"},
   {name: "Rebuttel Chardonnay", producer: "Rebuttel", vintage: 2019, blend: "Chardonnay", price: "6.99"},
@@ -6,7 +13,9 @@ Wine.create!([
 ])
 UserWine.create!([
   {favorite: false, user_id: 1, wine_id: 3},
-  {favorite: false, user_id: 1, wine_id: 2}
+  {favorite: false, user_id: 1, wine_id: 2},
+  {favorite: false, user_id: 2, wine_id: 1},
+  {favorite: false, user_id: 2, wine_id: 4}
 ])
 User.create!([
   {name: "Michael", email: "michael@test.com", password_digest: "$2a$12$gCG9HzT9DJ67GGbXmGs00O5oghLgTWQp.Cl.0BGGPokq7/3yrOx6i"},
@@ -16,7 +25,15 @@ Origin.create!([
   {country: "France", region: "Pays d'Oc", appellation: nil},
   {country: "Portugal", region: "Vinho Verde", appellation: nil},
   {country: "United States", region: "California", appellation: nil},
-  {country: "Italy", region: "Veneto", appellation: "Conegliano Valdobbiadene"}
+  {country: "Italy", region: "Veneto", appellation: nil}
+])
+CategoryWine.create!([
+  {wine_id: 1, category_id: 5},
+  {wine_id: 1, category_id: 2},
+  {wine_id: 3, category_id: 1},
+  {wine_id: 4, category_id: 5},
+  {wine_id: 4, category_id: 3},
+  {wine_id: 2, category_id: 2}
 ])
 Category.create!([
   {name: "Red"},
