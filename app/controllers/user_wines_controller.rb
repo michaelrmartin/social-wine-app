6225eaaddd8b8e74e2651d62c4de2a6f511f11a5
michaelrmartin <iammichaelmartin@gmail.com>
2022-09-04 13:58:19 -0500

    Updates views form users and wines

--- app/controllers/user_wines_controller.rb
+++ app/controllers/user_wines_controller.rb
@@ -10,7 +10,7 @@ class UserWinesController < ApplicationController
     user_wine = UserWine.new(
       user_id: current_user.id,
       wine_id: params[:wine_id], 
-      favorite: false
+      favorite: params[:favorite]
     )
 
 
@@ -19,6 +19,33 @@ class UserWinesController < ApplicationController
     else
       render json: {errors: user_wine.errors.full_messages}, status: 422
     end
+
+    def update
+      wine_id = params["id"]
+      user_wine = UserWine.find_by(wine_id: wine_id)
+
+      user_wine.user_id = current_user.id || user_wine.user_id
+      user_wine.wine_id = params[:wine_id] || user_wine.wine_id 
+      user_wine.wine_id  = params[:favorite] || user_wine.wine_id
+
+      if user_wine.save
+        @user_wine = user_wine
+        render json: user_wine.as_json
+      else 
+        render json: {errors: wine.errors.full_messages} ,
+        status: 422
+      end
+
+    end
+
+    def destroy
+      wine_id = params["id"]
+      user_wine = UserWine.find_by(wine_id: wine_id)
+      uswer_wine.destroy
+      render json: {message: "Wine successfully destroyed."}
+    end
+  
+
 
   end
 
